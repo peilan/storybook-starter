a301e0f03e0777dae6ab648a858d7671e1d47a7b
peilan <bl-74@yandex.ru>
2018-05-13 10:27:17 +0500

    added actions in stories

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
 export default(props) => {
-  const { value, enabled = true } = props
+  const { value, enabled = true, onClick } = props
 
   const styles = {
     border: '1px solid yellow'
   }
 
+  const handleClick = e => {
+    e.preventDefault()
+    onClick && onClick()
+  }
+
   return (
-    <button style={styles} disabled={!enabled}>{value}</button>
+    <button style={styles} onClick={handleClick} disabled={!enabled}>{value}</button>
   )
 }
--- src/components/button/Button.stories.js
+++ src/components/button/Button.stories.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 import { withKnobs, text, boolean } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions';
 
 import Button from './Button'
 
@@ -9,5 +10,9 @@ const stories = storiesOf('Button', module)
 stories.addDecorator(withKnobs)
 
 stories.add('With knobs', () => (
-  <Button value={text('Value', 'simple   text')} enabled={boolean('Enabled', true)} />
+  <Button
+    value={text('Value', 'simple   text')}
+    onClick={action('onClick')}
+    enabled={boolean('Enabled', true)}
+  />
 ))
